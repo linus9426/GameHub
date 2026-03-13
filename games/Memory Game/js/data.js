4d1e62ca30196b9c1c69f409c817b15a3f86149b
Federico <federicolinus@hotmail.com>
2026-03-13 10:11:18 +0000

    Update data.js

--- games/Memory Game/js/data.js
+++ games/Memory Game/js/data.js
@@ -1,19 +1,19 @@
 // Define the unique cards
 export const cardsArray = [
-  { name: "buffalo", img: "./assets/images/buffalo.png" },
-  { name: "crocodile-2", img: "./assets/images/crocodile-2.png" },
-  { name: "crocodile", img: "./assets/images/crocodile.png" },
-  { name: "rabbit", img: "./assets/images/rabbit.png" },
-  { name: "cow", img: "./assets/images/cow.png" },
-  { name: "flamingo", img: "./assets/images/flamingo.png" },
-  { name: "fox", img: "./assets/images/fox.png" },
-  { name: "giraffe-2", img: "./assets/images/giraffe-2.png" },
-  { name: "giraffe", img: "./assets/images/giraffe.png" },
-  { name: "horse", img: "./assets/images/horse.png" },
-  { name: "pig", img: "./assets/images/pig.png" },
-  { name: "hedgehog", img: "./assets/images/hedgehog.png" },
-  { name: "lion", img: "./assets/images/lion.png" },
-  { name: "hippo", img: "./assets/images/hippo.png" },
-  { name: "sheep", img: "./assets/images/sheep.png" }
+  { name: "buffalo", img: "assets/images/buffalo.png" },
+  { name: "crocodile-2", img: "assets/images/crocodile-2.png" },
+  { name: "crocodile", img: "assets/images/crocodile.png" },
+  { name: "rabbit", img: "assets/images/rabbit.png" },
+  { name: "cow", img: "assets/images/cow.png" },
+  { name: "flamingo", img: "assets/images/flamingo.png" },
+  { name: "fox", img: "assets/images/fox.png" },
+  { name: "giraffe-2", img: "assets/images/giraffe-2.png" },
+  { name: "giraffe", img: "assets/images/giraffe.png" },
+  { name: "horse", img: "assets/images/horse.png" },
+  { name: "pig", img: "assets/images/pig.png" },
+  { name: "hedgehog", img: "assets/images/hedgehog.png" },
+  { name: "lion", img: "assets/images/lion.png" },
+  { name: "hippo", img: "assets/images/hippo.png" },
+  { name: "sheep", img: "assets/images/sheep.png" }
 ];
 
